ab438912af627bf21cbade450a2250d979989d04
Techj70 <65133090+Techj70@users.noreply.github.com>
2021-10-05 17:13:29 -0400

    setup arrays for socket listeners and switches for determining which shape is placed in the sockets in in Shape3DPuzzle script

--- Assets/Scripts/Shape3DPuzzle.cs
+++ Assets/Scripts/Shape3DPuzzle.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
@@ -7,12 +8,26 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Shape3DPuzzle : MonoBehaviour
 {
     private int shapeNumber;
+    private int arrayX = 1; //used to start array at 1 for easy of use with socket numbers in the editor
     
-    public XRBaseInteractable socketInteractor;
-    
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] public XRSocketInteractor[] socket;
+    [SerializeField] public string[] code;
+
+    public Shape3D socketedShape;
+
+    private void Start()
     {
+    }
+
+    void Awake()
+    {
+        while (arrayX <= 27)
+        {
+            socket[arrayX].onSelectEntered.AddListener(CheckShape);
+            socket[arrayX].onSelectExited.AddListener(ClearCodeNumber);
+            arrayX++;
+        }
+        
     }
 
     // Update is called once per frame
@@ -28,7 +43,34 @@ public class Shape3DPuzzle : MonoBehaviour
          get shape of placedShape by checking its components for a shape script and have a switch use it to determine what number to assign to shapeNumber
          have another switch determine which value to add to to enteredCodeNumber using shapeNumber and socketNumber        
         */
-    }
+        socketedShape = placedShape.GetComponent<Shape3D>();
+        switch (socketedShape.shapeName)
+        {
+            case "Cube":
+                Debug.Log("cube placed");
+                break;
+            case "RectSolid":
+                Debug.Log("rectangle placed");
+                break;
+            case "PrismTall":
+                Debug.Log("prism tall placed");
+                break;
+            case "PrismLong":
+                Debug.Log("prism long placed");
+                break;
+            case "Pyramid":
+                Debug.Log("pyramid placed");
+                break;
+            case "Sphere":
+                Debug.Log("sphere placed");
+                break;
+            case "Cylinder":
+                Debug.Log("cylinder placed");
+                break;
+            
+        }
+    
+}
     
     public void ClearCodeNumber(XRBaseInteractable placedShape)
     {
