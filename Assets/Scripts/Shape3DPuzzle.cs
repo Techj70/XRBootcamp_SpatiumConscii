f3c0ed895af7cf3f9e940144cb1c52ff15bddf25
Techj70 <65133090+Techj70@users.noreply.github.com>
2021-10-06 16:42:08 -0400

    completed solve logic/code for code completion; fixed error with socketNumber not clearing (had to set SelectExited event on each socket to call GetSocketNumber like the Select event does); setup for one shape puzzle

--- Assets/Scripts/Shape3DPuzzle.cs
+++ Assets/Scripts/Shape3DPuzzle.cs
@@ -2,22 +2,31 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using Random = System.Random;
 
 public class Shape3DPuzzle : MonoBehaviour
 {
     private int shapeNumber;
     private int arrayX = 1; //used to start array at 1 for easy of use with socket numbers in the editor
-
+    private int i = 1;
     private int socketNumber = 0;
     
+    public bool codeCorrect;
+
     [SerializeField] public XRSocketInteractor[] socket;
     [SerializeField] public string[] codeSolution; //the code need to solve the puzzle
     [SerializeField] public string[] code; //the code inputted by the user
-    
+
+    [SerializeField]
+    public string[] shapeDigit; //the shape numbers used to populate the code array above; each corresponse to a shape
+
 
     public Shape3D socketedShape;
+    public string codeSolutionString; //the codeSolution array made into one string variable
+    public int moveCounter;
 
     private void Start()
     {
@@ -25,19 +34,19 @@ public class Shape3DPuzzle : MonoBehaviour
 
     void Awake()
     {
+        moveCounter = 0;
         while (arrayX <= 27)
         {
             socket[arrayX].onSelectEntered.AddListener(CheckShape);
             socket[arrayX].onSelectExited.AddListener(ClearCodeNumber);
             arrayX++;
         }
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void CheckShape(XRBaseInteractable placedShape)
@@ -47,6 +56,7 @@ public class Shape3DPuzzle : MonoBehaviour
          get shape of placedShape by checking its components for a shape script and have a switch use it to determine what number to assign to shapeNumber
          have another switch determine which value to add to to enteredCodeNumber using shapeNumber and socketNumber        
         */
+        moveCounter++;
         socketedShape = placedShape.GetComponent<Shape3D>();
 
         switch (socketedShape.shapeName)
@@ -55,80 +65,91 @@ public class Shape3DPuzzle : MonoBehaviour
                 Debug.Log("cube placed");
                 if (socketNumber <= 0)
                 {
-                    Debug.Log("error, socket number read as 0"); 
+                    Debug.Log("error, socket number read as 0");
                     break;
                 }
                 code[socketNumber] = "1";
-                Debug.Log("code["+ socketNumber +"] set to 1");
+                Debug.Log("code[" + socketNumber + "] set to 1");
                 break;
+            
             case "RectSolid":
                 Debug.Log("rectangle placed");
                 if (socketNumber <= 0)
                 {
-                    Debug.Log("error, socket number read as 0"); 
+                    Debug.Log("error, socket number read as 0");
                     break;
                 }
                 code[socketNumber] = "2";
-                Debug.Log("code["+ socketNumber +"] set to 2");
+                Debug.Log("code[" + socketNumber + "] set to 2");
                 break;
+            
             case "PrismTall":
                 Debug.Log("prism tall placed");
                 if (socketNumber <= 0)
                 {
-                    Debug.Log("error, socket number read as 0"); 
+                    Debug.Log("error, socket number read as 0");
                     break;
                 }
                 code[socketNumber] = "3";
-                Debug.Log("code["+ socketNumber +"] set to 3");
+                Debug.Log("code[" + socketNumber + "] set to 3");
                 break;
+            
             case "PrismLong":
                 Debug.Log("prism long placed");
                 if (socketNumber <= 0)
                 {
-                    Debug.Log("error, socket number read as 0"); 
+                    Debug.Log("error, socket number read as 0");
                     break;
                 }
                 code[socketNumber] = "4";
-                Debug.Log("code["+ socketNumber +"] set to 4");
+                Debug.Log("code[" + socketNumber + "] set to 4");
                 break;
+            
             case "Pyramid":
                 Debug.Log("pyramid placed");
                 if (socketNumber <= 0)
                 {
-                    Debug.Log("error, socket number read as 0"); 
+                    Debug.Log("error, socket number read as 0");
                     break;
                 }
                 code[socketNumber] = "5";
-                Debug.Log("code["+ socketNumber +"] set to 5");
+                Debug.Log("code[" + socketNumber + "] set to 5");
                 break;
+            
             case "Sphere":
                 Debug.Log("sphere placed");
                 if (socketNumber <= 0)
                 {
-                    Debug.Log("error, socket number read as 0"); 
+                    Debug.Log("error, socket number read as 0");
                     break;
                 }
                 code[socketNumber] = "6";
-                Debug.Log("code["+ socketNumber +"] set to 6");
+                Debug.Log("code[" + socketNumber + "] set to 6");
                 break;
+            
             case "Cylinder":
                 Debug.Log("cylinder placed");
                 if (socketNumber <= 0)
                 {
-                    Debug.Log("error, socket number read as 0"); 
+                    Debug.Log("error, socket number read as 0");
                     break;
                 }
+
                 code[socketNumber] = "7";
-                Debug.Log("code["+ socketNumber +"] set to 7");
+                Debug.Log("code[" + socketNumber + "] set to 7");
                 break;
-            
+
         }
 
-        Debug.Log("current code is " + code);
-
+        //Debug.Log("current code is ");
+        
+        if (CheckSolution() == true)
+        { Debug.Log("puzzle solved!"); }
+        else
+        { Debug.Log("puzzle not solved"); }
     }
 
-    public void GetSocketNumber (int receivedSocketNumber)
+    public void GetSocketNumber(int receivedSocketNumber)
     {
         socketNumber = receivedSocketNumber;
     }
@@ -139,14 +160,29 @@ public class Shape3DPuzzle : MonoBehaviour
          get socketNumber from the XR Socket Interactable component that this function should be called from
          have a switch determine which number place to set to 0 in enteredCodeNumber using socketNumber        
         */
+        moveCounter++;
         code[socketNumber] = "0";
-        Debug.Log(("shape removed from " + socketNumber+ " and code[" + socketNumber + "] set to 0"));
+        Debug.Log(("shape removed from " + socketNumber + " and code[" + socketNumber + "] set to 0"));
     }
 
-    public void CheckSolution()
+    public bool CheckSolution()
     {
-        if (code == codeSolution)
+        Debug.Log("CheckSolution called");
+        
+        i = 1;
+        while (i <= 27)
         {
+            if (code[i] == codeSolution[i])
+            { codeCorrect = true; }
+            else
+            {
+                codeCorrect = false;
+                break;
+            }
+
+            i++;
         }
+        return codeCorrect;
     }
+
 }
